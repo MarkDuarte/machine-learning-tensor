ec050c88bc5be22b831fdc2dd7106402972521e6
Marcos Duarte <marcosduarte1994@gmail.com>
2023-02-17 15:48:33 -0300

    feat: changes validations in gestures

--- src/index.js
+++ src/index.js
@@ -76,16 +76,19 @@
           }
 
           const keypoints3D = hand.keypoints3D.map(keypoint => [keypoint.x, keypoint.y, keypoint.z])
-          const est = GE.estimate(keypoints3D, 9)
-          if (est.gestures.length > 0) {
+          const predictions = GE.estimate(keypoints3D, 9)
+          if (!predictions.gestures.length) {
+            updateDebugInfo(predictions.poseData, 'left')
+          }
+          if (predictions.gestures.length > 0) {
 
             // find gesture with highest match score
-            let result = est.gestures.reduce((p, c) => {
+            let result = predictions.gestures.reduce((p, c) => {
               return (p.score > c.score) ? p : c
             })
             const chosenHand = hand.handedness.toLowerCase()
             resultLayer[chosenHand].innerText = gestureStrings[result.name]
-            updateDebugInfo(est.poseData, chosenHand)
+            updateDebugInfo(predictions.poseData, chosenHand)
           }
 
         }
